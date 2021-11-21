4d9876a50f7276f1772b35d6270114090f57f906
Lednev Eugene <lednev.eugene@gmail.com>
2021-11-21 04:32:53 +0200

    ci: 🎡 Test github webhook

--- BudgetAssistantBackend/BudgetAssistant.API/Controllers/WeatherForecastController.cs
+++ BudgetAssistantBackend/BudgetAssistant.API/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace BudgetAssistant.API.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 100),
+                TemperatureC = rng.Next(-20, 101),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();

50918703be0adea1a7938d2e17c0308ef2b56f43
Eugene <lednev.eugene@gmail.com>
2023-05-01 01:15:49 +0200

    test: 💍 122

--- assistants-budget-ops/develop/main.tf
+++ assistants-budget-ops/develop/main.tf
@@ -1,5 +1,5 @@
 module "s3" {
   source                              = "../modules/assistants-budget-ui-s3"
   common_tags = local.common_tags
-  bucket_name = "assistants-budget-ui-s3-${loca.common_tags.environment}"
+  bucket_name = "assistants-budget-ui-s3-${local.common_tags.environment}"
 }

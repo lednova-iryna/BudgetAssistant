895b629e409cae5d412c80740db3ec65cba96f4b
Eugene <lednev.eugene@gmail.com>
2023-05-01 18:33:39 +0200

    test: 💍 fff

--- actions/react-deploy-to-s3/entrypoint.sh
+++ actions/react-deploy-to-s3/entrypoint.sh
@@ -55,9 +55,8 @@ echo "Working directory consists of: $(ls)"
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-# sh -c "yarn" \
-# &&
-sh -c "${NODE_ENV_PREPEND} yarn build" &&
+sh -c "yarn install" &&
+  sh -c "${NODE_ENV_PREPEND} yarn build" &&
   sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3 \
               --no-progress \

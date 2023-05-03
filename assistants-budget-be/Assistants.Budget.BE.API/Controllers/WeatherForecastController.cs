42aa56bde37df9387eb22b22e03002398d3bf021
Eugene <lednev.eugene@gmail.com>
2023-05-03 22:28:39 +0200

    feat: 🎸 Transactions
    Setup basic BackEnd project. Add basic Transactions GET API.

---
+++ assistants-budget-be/Assistants.Budget.BE.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Mvc;
+using Assistants.Budget.BE.Domain;
+using Assistants.Budget.BE.Mediator.Transactions;
+
+namespace Assistants.Budget.BE.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TransactionController : ControllerBase
+{
+    private readonly ILogger<TransactionController> logger;
+    private readonly IMediator mediator;
+
+    public TransactionController(ILogger<TransactionController> logger, IMediator mediator)
+    {
+        this.logger = logger;
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Transaction>> Get()
+    {
+        return await mediator.Send(new TransactionsQuery());
+    }
+}
+
---
+++ assistants-budget-be/Assistants.Budget.BE.Domain/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace Assistants.Budget.BE.Domain;
+public class Transaction
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime Date { get; set; }
+    public double Amount { get; set; }
+    public string Note { get; set; }
+    public TransactionType Type { get; set; }
+
+}
+

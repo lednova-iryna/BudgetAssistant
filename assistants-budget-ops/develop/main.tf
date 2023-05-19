c85f0c0bdbd5c23e56a702e9699cf5476e7bc836
Eugene <lednev.eugene@gmail.com>
2023-05-19 18:14:27 +0200

    test: 💍 fff

--- assistants-budget-ops/develop/main.tf
+++ assistants-budget-ops/develop/main.tf
@@ -1,4 +1,4 @@
-module "assistants_budget_ui_distribution" {
+module "assistants-budget-ui-distribution" {
   source                         = "./modules/assistants-budget-ui-distribution"
   common_tags                    = local.common_tags
   bucket_name                    = var.assistants_budget_ui_s3_bucket_name
@@ -7,19 +7,19 @@ module "assistants_budget_ui_distribution" {
   aws_route53_hostedzone_id      = var.assistants_budget_ui_route53_hostedzone_id
 }
 
-module "assistants_budget_be_api_lambda" {
+module "assistants-budget-be-api-lambda" {
   source      = "./modules/assistants-budget-be-api-lambda"
   common_tags = local.common_tags
 
   aws_lambda_architecture     = "arm64"
-  aws_lambda_docker_image_url = ""
+  aws_lambda_docker_image_url = "place_holder"
   aws_lambda_memory_size      = 256
   aws_lambda_parameter_name   = "/develop/assistants-budget-be-api-lambda"
   aws_lambda_timeout          = 60
   aws_lambda_name             = "assistants-budget-be-api-lambda-develop"
 }
 
-module "assistants_budget_be_api_gateway" {
+module "assistants-budget-be-api-gateway" {
   source                                        = "./modules/aws-gateway-http-api"
   common_tags                                   = local.common_tags
   api_gateway_http_api_name                     = "Assistants: Budget. Develop"

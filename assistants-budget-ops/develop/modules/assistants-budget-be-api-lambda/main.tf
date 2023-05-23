0f0f9101f748f1b0a991cd6aa417ee7f645df509
Eugene <lednev.eugene@gmail.com>
2023-05-23 11:00:56 +0200

    ci: 🎡 Budget.API
    Lambda CloudWatch policies

--- assistants-budget-ops/develop/modules/assistants-budget-be-api-lambda/main.tf
+++ assistants-budget-ops/develop/modules/assistants-budget-be-api-lambda/main.tf
@@ -1,4 +1,5 @@
 data "aws_iam_policy_document" "assume_role" {
+
   statement {
     effect = "Allow"
 
@@ -13,9 +14,42 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "cloudwatch_policy_doc" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+resource "aws_iam_policy" "cloudwatch_policy" {
+  name   = "lambda_cloudwatch_policy"
+  policy = data.aws_iam_policy_document.cloudwatch_policy_doc.json
+  tags = merge(
+    var.common_tags,
+    {
+      project = "assistants-budget-be-api"
+    }
+  )
+}
+
 resource "aws_iam_role" "lambda_iam_role" {
   name               = "lambda_iam_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags = merge(
+    var.common_tags,
+    {
+      project = "assistants-budget-be-api"
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "audit_trail_get_configuration_lambda_ecr_access_role" {
+  role       = aws_iam_role.lambda_iam_role.name
+  policy_arn = aws_iam_policy.cloudwatch_policy.arn
 }
 
 #### AWS Lambda ####

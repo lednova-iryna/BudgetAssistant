46111be2cc6a63e4801ccbff5e7e95c6ba5d3b04
Irina Lednova <80521360+lednova-irina@users.noreply.github.com>
2023-05-26 15:00:38 +0200

    Feature/ecr (#9)
    * ci: 🎡 Ops
    
    Set Budget.API ECR repo policy to remove old images, left only latest
    one
    
    ---------
    
    Co-authored-by: Eugene <lednev.eugene@gmail.com>

--- assistants-budget-ops/develop/modules/ecr/main.tf
+++ assistants-budget-ops/develop/modules/ecr/main.tf
@@ -6,3 +6,33 @@ resource "aws_ecr_repository" "this" {
     scan_on_push = true
   }
 }
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+  policy = jsonencode({
+    rules : [{
+      "rulePriority" : 2,
+      "description" : "Expire `develop` images older than 1 day and not latest",
+      "selection" : {
+        "tagStatus" : "tagged",
+        "tagPrefixList" : ["develop-"]
+        "countType" : "imageCountMoreThan",
+        "countNumber" : 1
+      },
+      "action" : {
+        "type" : "expire"
+      }
+      }, {
+      "rulePriority" : 1,
+      "description" : "Expire `production` images older than 1 day and not latest",
+      "selection" : {
+        "tagStatus" : "tagged",
+        "tagPrefixList" : ["production-"]
+        "countType" : "imageCountMoreThan",
+        "countNumber" : 1
+      },
+      "action" : {
+        "type" : "expire"
+      }
+    }]
+  })
+}
